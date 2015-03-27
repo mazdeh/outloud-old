847127dd9c36f284415f4dc2a0c544fb3d6c4d8a
mazdeh <vahid.mazdeh@gmail.com>
2015-03-26 20:42:30 -0600

    testing server auth

--- private/soundcloud.js
+++ private/soundcloud.js
@@ -2,3 +2,7 @@ SC.initialize({
   client_id: "5eaf39e81aa342b9d5015a999666cbe1",
   redirect_uri: "https://theoutloud.herokuapp.com/callback.html",
 });
+
+SC.connect(function(){
+	SC.oEmbed("https://soundcloud.com/jozaytnb/fetty-wap-my-way-feat-monty", document.getElementById("embed"));
+});
